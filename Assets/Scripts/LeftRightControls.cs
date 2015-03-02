0b2a908dbe0af8bedb14d2afb20323557c845cf9
hedii-mejri <hedi.mejri@esprit.tn>
2015-03-02 12:00:23 +0100

    Commit
    Initial Commit

---
+++ Assets/Scripts/LeftRightControls.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LeftRightControls : MonoBehaviour {
+	public KeyCode moveLeft = KeyCode.LeftArrow;
+	public KeyCode moveRight = KeyCode.RightArrow;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		for (var i = 0; i < Input.touchCount; ++i) {
+			Touch touch = Input.GetTouch(i);
+			if (touch.phase == TouchPhase.Began || Input.GetKey(moveLeft)) {
+				//if (touch.position.x > (Screen.width/2)) {
+					Debug.Log(touch.position);
+				//}
+			}
+		}
+		if (Input.GetKey (moveLeft)) {
+
+			var paddle1 = GameObject.Find("PaddleU");
+			paddle1.transform.Translate (0,-0.1f,0);
+			
+			var paddle2 = GameObject.Find("PaddleB");
+			paddle2.transform.Translate (0,-0.1f,0);
+
+		}
+		else if (Input.GetKey (moveRight)) {
+
+			var paddle1 = GameObject.Find("PaddleU");
+			paddle1.transform.Translate (0,0.1f,0);
+			
+			var paddle2 = GameObject.Find("PaddleB");
+			paddle2.transform.Translate (0,0.1f,0);
+		}
+		else if (!Input.anyKey) {
+			var velocity = rigidbody2D.velocity;
+			velocity.y = 0.0f;
+			rigidbody2D.velocity = velocity;
+		}
+		
+		var reset = rigidbody2D.velocity;
+		reset.x = 0;
+		rigidbody2D.velocity = reset;
+	}
+}
---
+++ Assets/Scripts/UpDownControls.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UpDownControls : MonoBehaviour {
+
+	public KeyCode moveUp = KeyCode.UpArrow;
+	public KeyCode moveDown = KeyCode.DownArrow;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (moveUp)) {
+			var paddle1 = GameObject.Find("PaddleL");
+			paddle1.transform.Translate (0,0.1f,0);
+			
+			var paddle2 = GameObject.Find("PaddleR");
+			paddle2.transform.Translate (0,-0.1f,0);
+		}
+		else if (Input.GetKey (moveDown)) {
+			var paddle1 = GameObject.Find("PaddleL");
+			paddle1.transform.Translate (0,-0.1f,0);
+			
+			var paddle2 = GameObject.Find("PaddleR");
+			paddle2.transform.Translate (0,0.1f,0);
+		}
+		else if (!Input.anyKey) {
+			var velocity = rigidbody2D.velocity;
+			velocity.y = 0.0f;
+			rigidbody2D.velocity = velocity;
+		}
+
+		var reset = rigidbody2D.velocity;
+		reset.x = 0;
+		rigidbody2D.velocity = reset;
+	}
+}
